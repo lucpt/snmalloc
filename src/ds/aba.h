bb81acc4c83d09ce24256be7f9efcc1dc0f35b0f
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2019-05-14 12:16:20 +0100

    ds/aba: prototype CHERI MIPS LL/SC implementation

--- src/ds/aba.h
+++ src/ds/aba.h
@@ -29,6 +29,8 @@ namespace snmalloc
       "Expecting ABA to be the size of two pointers");
 
     using Cmp = Linked;
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+    using Cmp = T*;
 #else
     using Cmp = T*;
 #endif
@@ -40,6 +42,8 @@ namespace snmalloc
       alignas(2 * sizeof(std::size_t)) std::atomic<Linked> linked;
       Independent independent;
     };
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+    T* raw;
 #else
     std::atomic<T*> raw;
 #endif
@@ -56,6 +60,8 @@ namespace snmalloc
 #ifdef PLATFORM_IS_X86
       independent.ptr.store(x, std::memory_order_relaxed);
       independent.aba.store(0, std::memory_order_relaxed);
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+      raw = x;
 #else
       raw.store(x, std::memory_order_relaxed);
 #endif
@@ -66,6 +72,8 @@ namespace snmalloc
       return
 #ifdef PLATFORM_IS_X86
         independent.ptr.load(std::memory_order_relaxed);
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+        raw;
 #else
         raw.load(std::memory_order_relaxed);
 #endif
@@ -73,12 +81,18 @@ namespace snmalloc
 
     Cmp read()
     {
-      return
 #ifdef PLATFORM_IS_X86
-        Cmp{independent.ptr.load(std::memory_order_relaxed),
-            independent.aba.load(std::memory_order_relaxed)};
+      return Cmp{independent.ptr.load(std::memory_order_relaxed),
+                 independent.aba.load(std::memory_order_relaxed)};
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+      T* tp;
+      asm("cllc %[tp], %[raw]\n"
+          : [tp] "=C"(tp)
+          : [raw] "C"(&this->raw)
+          : "memory");
+      return tp;
 #else
-        raw.load(std::memory_order_relaxed);
+      return raw.load(std::memory_order_relaxed);
 #endif
     }
 
@@ -109,6 +123,25 @@ namespace snmalloc
       return linked.compare_exchange_weak(
         expect, xchg, std::memory_order_relaxed, std::memory_order_relaxed);
 #  endif
+#elif defined(__mips__) && defined(__CHERI_PURE_CAPABILITY__)
+      int res;
+      T* tv = this->raw;
+      T* e = ptr(expect);
+      asm(
+        "cexeq %[res], %[tv], %[expect]\n\t"
+        "beqz %[res], 1f\n\t"
+        "nop\n\t"
+        "cscc %[res], %[value], %[raw]\n\t"
+        "1:\n"
+        : [res] "=r"(res)
+        :
+        [raw] "C"(&this->raw), [tv] "C"(tv), [expect] "C"(e), [value] "C"(value)
+        : "memory");
+      if (!res)
+      {
+        expect = read();
+      }
+      return static_cast<bool>(res);
 #else
       return raw.compare_exchange_weak(
         expect, value, std::memory_order_relaxed, std::memory_order_relaxed);
